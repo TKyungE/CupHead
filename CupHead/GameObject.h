1c820b47caf53d4d3de3138a8d3824f51d39c2a8
Instructer <gomdi202@naver.com>
2025-03-28 17:07:11 +0900

    Add Actor, Character, Pawn

--- CupHead/GameObject.h
+++ CupHead/GameObject.h
@@ -5,9 +5,9 @@ class GameObject
 {
 public:
 	GameObject();
-	~GameObject();
+	virtual ~GameObject();
 
-	void Init();											// 멤버 변수의 초기화, 메모리 할당
+	virtual void Init();											// 멤버 변수의 초기화, 메모리 할당
 	virtual void Release();									// 메모리 해제
 	virtual void Update();									// 프레임 단위로 게임 로직 실행(데이터 계산)
 	virtual void Render(HDC hdc);							// 프레임 단위로 출력(이미지, 텍스트 등)
@@ -16,6 +16,7 @@ public:
 
 	FPOINT GetPos() const { return pos; }
 	bool IsDead() const { return bDead; }
+
 protected:
 	FPOINT pos;
 	FPOINT size;
